55510e5adf28560ef292dfbfe70965d3f7c03ebb
htmlbasic <withjanak@gmail.com>
2025-06-12 14:15:07 +0545

    node_modules added

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express')
  const app = express()
 
+ app.get("/",(req,res)=>{
+    
+    res.json({
+        message: "This is home Page"
+    })
+})
+ app.get("/about",(req,res)=>{
+    
+    res.json({
+        message: "About Page"
+    })
+})
+
  app.listen(3000,()=>{
     console.log("NodeJs project has started")
  })
+//  mongodb+srv://withjanak:<db_password>@cluster0.j7iiuep.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
